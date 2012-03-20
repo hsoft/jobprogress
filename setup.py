b4a54eafd99050ff7fa4b89eccf9aa248088a2d3
Virgil Dupras <hsoft@hardcoded.net>
2012-03-20 10:12:01 -0400

    v1.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESC = open('README', 'rt').read() + '\n\n' + open('CHANGES', 'rt').read()
 
 setup(
     name='jobprogress',
-    version='1.0.3',
+    version='1.0.4',
     author='Hardcoded Software',
     author_email='hsoft@hardcoded.net',
     packages=['jobprogress'],
